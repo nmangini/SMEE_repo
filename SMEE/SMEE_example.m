693491622c150fecc5709db67a564fcd4213d829
nick <nmangini@umass.edu>
2014-01-06 16:41:13 -0500

    Added further changes to SMEE example run

--- SMEE/SMEE_example.m
+++ SMEE/SMEE_example.m
@@ -7,30 +7,30 @@
 % output files to see how well the code performed
 
 % NOTE: SMEE is hard coded to run with a specific directory setup:
-    % ~/SMEE_BBH/
-    % ~/SMEE_BBH/SMEE/
-    % ~/SMEE_BBH/SMEE/Results
-    % ~/SMEE_BBH/PCA/
-    % ~/SMEE_BBH/PCA/Q-series
-    % ~/SMEE_BBH/Waveforms/Q-series
+    % /data/nmangini/SMEE_BBH/
+    % /data/nmangini/SMEE_BBH/SMEE/
+    % /data/nmangini/SMEE_BBH/SMEE/Results
+    % /data/nmangini/SMEE_BBH/PCA/
+    % /data/nmangini/SMEE_BBH/PCA/Q-series
+    % /data/nmangini/SMEE_BBH/Waveforms/Q-series
     
 % Creating the principle components:
     % See these scripts's comments for explanation of all steps
-    cd ~/SMEE_BBH/PCA/Q-series/Q_allmodes
-    Q_allmodes_loadmatrix.m
-    Q_allmodes_pca.m
-    Q_allmodes_match.m % optional
-    Q_allmodes_resample.m
+    cd /data/nmangini/SMEE_repo/PCA/Q-series/Q_allmodes
+    Q_allmodes_loadmatrix % CHANGE PATH NAMES
+    Q_allmodes_pca
+    Q_allmodes_match % optional
+    resampleQ
     
 % Post-processing:
-    load_frame.m
-    movefile('finalRvectorsPC_Q-series.mat','~/SMEE_BBH/SMEE/')
-    align_MDCs.m
-    movefile('final-MDC_Q-series.mat','~/SMEE_BBH/SMEE/')
-    cd ~/SMEE_BBH/SMEE/    
+    load_frame
+    copyfile('finalRvectorsPC_Q-series.mat','/data/nmangini/SMEE_repo/SMEE/')
+    align_MDCs
+    copyfile('final-MDC_Q-series.mat','/data/nmangini/SMEE_repo/SMEE/')
+    cd /data/nmangini/SMEE_repo/SMEE/    
 
 % Running SMEE:    
-    SMEE_BBH('Q1-Q-seed13-snr10-pcs8','aligo','Q','Q',1,10,13,1,8,0,'SNR',10);
+    SMEE_BBH('EXAMPLE-Q1-Q-seed13-snr10-pcs8','aligo','Q','Q',1,10,13,1,8,0,'SNR',10); % CHANGE PATH NAMES
     
     % NOTE: This script can take anywhere from 15 minutes to a couple hours
     % to finish running depending on the complexity of the signals and the
@@ -39,8 +39,8 @@
 % Plotting Results
     % These scripts are still under development to be more general/robust
     % so some of these scripts may not work
-    cd ~/SMEE_BBH/SMEE/Results/
-    collect_results.m % plots Bayes factors
-    reconstruction.m % reconstructs signal from beta factors and principle 
+    %cd /data/nmangini/SMEE_BBH/SMEE/Results/
+    %collect_results.m % plots Bayes factors
+    %reconstruction.m % reconstructs signal from beta factors and principle 
                      % components
-    plot_params.m % plots beta values of nested sampling loop
+    %plot_params.m % plots beta values of nested sampling loop
