6f35bceb1faeef3383f037a83cdc7eaf97a1ec05
nmangini <nmangini@umass.edu>
2014-03-04 21:15:34 -0500

    updated html format

--- Utilities/make_html.sh
+++ Utilities/make_html.sh
@@ -7,10 +7,10 @@
 # --- Define some environment variables to contain appropriately named
 # directories etc...
 
-injection_name="RO311" # Injection catalogue and waveform number
-catalogue_name="RO3" # Reconstruction catalogue
-filename=${injection_name}-${catalogue_name}
-current_directory=pwd
+injection_name="HR13" # Injection catalogue and waveform number
+injection="HR"
+filename=${injection_name}-resultspage
+current_directory=$(pwd)
 logB=
 SNR=10
 numactive=50
@@ -20,14 +20,21 @@ nits=10
 echo """
 <html>
 
-<h1>Injection: ${injection_name}, Catalogue: ${catalogue_name}</h1>
+<h1>Injection: ${injection_name}</h1>
 
-<h1>Input Information</h1>
+<h2>Input Information</h2>
 <ul>
 <li>SNR = ${SNR}</li>
 <li>Live Points = ${numactive}</li>
 <li>MCMC Samples = ${nits}</li>
 </ul>
+
+<h2>logB vs. Seed #</h2>
+<table>
+<tr>
+<td><img src="${current_directory}/${injection}-bayes.png"></td>
+</tr>
+</table>
 """ > ${filename}.html
 
 for seed in {1..20}
@@ -42,10 +49,17 @@ do
     <li>Seed = ${seed}</li>
     </ul>
     
-    <h2>Beta Posterior</h2>
+    <h2>Beta Posteriors</h2>
     <table>
     <tr>
-    <td><img src="${filename}-seed${seed}_postbetas.png"></td>
+    <th><b>Q Reconstruction</b></th>
+    <th><b>HR Reconstruction</b></th>
+    <th><b>RO3 Reconstruction</b></th>
+    </tr>
+    <tr>
+    <td><img src="${current_directory}/${injection_name}-Q-seed${seed}_postbetas.png"></td>
+    <td><img src="${current_directory}/${injection_name}-HR-seed${seed}_postbetas.png"></td>
+    <td><img src="${current_directory}/${injection_name}-RO3-seed${seed}_postbetas.png"></td>
     </tr>
     </table>
     """ >> ${filename}.html
@@ -53,5 +67,5 @@ do
    done
 
    # Call python plotting script to make figures:
-    python /data/nmangini/SMEE_repo/Utilities/plot_betas.py ${filename}-seed
+#    python /data/nmangini/SMEE_repo/Utilities/plot_betas.py ${filename}-seed
 
